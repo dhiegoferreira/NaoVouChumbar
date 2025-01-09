c2cbd29d15b1b01757ae336a1ce5a8a0f6225711
dhiegoferreira <dev.dhiegoferreira@gmail.com>
2025-01-09 20:07:57 +0000

    delete unused files

--- event-calendar-api/src/app.ts
+++ event-calendar-api/src/app.ts
@@ -18,8 +18,15 @@ app.get("/health", (req, res) => {
   res.status(200).json({ status: "OK" });
 });
 
-app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
-  errorHandler(err, req, res, next);
-} as express.ErrorRequestHandler);
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    errorHandler(err, req, res, next);
+  }
+);
 
 export { app };
